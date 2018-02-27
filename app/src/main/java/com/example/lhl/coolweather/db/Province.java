8fda3f19b33ef403767b2e31ecb817f483f7df1f
unknown <347311090@qq.com>
2018-02-27 14:31:54 +0800

    加入数据库和表的配置

---
+++ app/src/main/java/com/example/lhl/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.lhl.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lhl on 2018/2/27.
+ */
+
+public class Province extends DataSupport {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+}
